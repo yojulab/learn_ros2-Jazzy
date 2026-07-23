3089dd38b00548c2f6d9742f545ef0c63a4d8f1d
otter.oh <otter.oh@gmail.com>
2026-07-23 14:16:20 +0900

    Add initial package structure and example scripts
    - Created first_package with necessary files: __init__.py, first_node.py, package.xml, setup.py, setup.cfg, and resource directory.
    - Added example scripts for ROS2 functionality including turtlesim publisher, subscriber, and service client.
    - Included .gitignore for build artifacts and Python-related files.
    - Added install_useful_apps.sh for setting up development environment.

---
+++ 02_Intermediates/src/first_package/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'first_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sanghun-oh',
+    maintainer_email='otter.oh@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'first_node = first_package.first_node:main'
+        ],
+    },
+)
---
+++ install_useful_apps.sh
@@ -0,0 +1,2 @@
+sudo apt install -y curl git vim htop tmux wget unzip build-essential \
+    tree net-tools software-properties-common apt-transport-https ca-certificates gnupg-agent
