483b1e4397932e5549d350174e62407ecaa69b3e
otter.oh <otter.oh@gmail.com>
2026-07-23 15:39:19 +0900

    Add message dependencies and update setup.py for message installation

--- 02_Intermediates/src/first_package_msgs/setup.py
+++ 02_Intermediates/src/first_package_msgs/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Add the message files to be installed
+        ('share/' + package_name + '/msg', ['msg/CmdAndPoseVel.msg']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
